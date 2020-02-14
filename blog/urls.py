17732635015a6fe7cffadaab97058d606376e3a4
Alapakam Dorababu <dorababua67@gmail.com>
2020-02-14 19:29:01 +0530

    All function based views converted into class based views

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('', views.home, name="blog-home"),
-    path('post/create/', views.create_post, name="create-post"),
-    path('post/<int:id>/', views.post_details, name="post-detail"),
-    path('author/<str:author>/', views.author_posts, name="author"),
+    path('', views.HomeView.as_view(), name="blog-home"),
+    path('post/create/', views.CreatePostView.as_view(), name="create-post"),
+    path('post/delete/<int:id>/', views.PostDeleteView.as_view(), name="delete-post"),
+    path('post/<int:id>/', views.PostDetailView.as_view(), name="post-detail"),
+    path('author/<str:author>/', views.AuthorPostListView.as_view(), name="author"),
+   
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,43 +3,62 @@ from . models import Post
 from . forms import PostForm
 from django.contrib import messages
 from django.core.paginator import Paginator
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-def home(request):
-    posts = Post.objects.all().order_by('-date_posted')
-    paginator = Paginator(posts, 3)
-    page_num = request.GET.get('page',1)
-    page = paginator.page(page_num)
-    return render(request, 'blog/home.html', {'page_obj':page})
 
-@login_required
-def create_post(request):
-    form = PostForm()
-    if request.method == 'POST':
-        form = form = PostForm(request.POST)
+class HomeView(View):
+    def get(self, request):
+        posts = Post.objects.all().order_by('-date_posted')
+        paginator = Paginator(posts, 3)
+        page_num = request.GET.get('page',1)
+        page = paginator.page(page_num)
+        return render(request, 'blog/home.html', {'page_obj':page})
+
+
+class CreatePostView(LoginRequiredMixin ,View):
+    def get(self, request):
+         form = PostForm()
+         return render(request, 'blog/create-post.html', {'form':form})
+ 
+    def post(self, request):
+        form = PostForm(request.POST)
         if form.is_valid():
             newpost = form.save(commit=False)
             newpost.author = request.user
             form.save()
             messages.success(request, "Your post has been created!")
             return redirect('post-detail', id=newpost.id)
-    return render(request, 'blog/create-post.html', {'form':form})
 
+class PostDetailView(View):
+    def get(self, request, id=None):
+        post = Post.objects.get(id=id)
+        return render(request, 'blog/post-details.html', {'post':post})
 
-def post_details(request, id=None):
-    post = Post.objects.get(id=id)
-    return render(request, 'blog/post-details.html', {'post':post})
+class AuthorPostListView(View):
+    def get(self, request, author=None):
+        posts = Post.objects.filter(author__username__startswith = author).order_by('-date_posted')
+        paginator = Paginator(posts, 3)
+        page_num = request.GET.get('page',1)
+        page = paginator.page(page_num)
+        context = {
+            'page_obj':page, 
+            'posts': paginator.count,
+            'username': author,
+        }
+        return render(request, 'blog/author-posts.html', context)
 
-def author_posts(request, author=None):
-    posts = Post.objects.filter(author__username__startswith = author).order_by('-date_posted')
-    paginator = Paginator(posts, 3)
-    page_num = request.GET.get('page',1)
-    page = paginator.page(page_num)
-    context = {
-        'page_obj':page, 
-        'posts': paginator.count,
-        'username': author,
-    }
-    return render(request, 'blog/author-posts.html', context)
+class PostDeleteView(View):
+    def get(self, request, id=None):
+        post = Post.objects.get(id=id)
+        return render(request, 'blog/post-delete.html', {'post':post})
+        
+    def post(self, request, id=None):
+        post = Post.objects.get(id=id)
+        Post.delete(post)
+        return redirect('blog-home')
+
