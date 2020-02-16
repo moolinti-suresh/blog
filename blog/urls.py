cb169fc738b60685e5a2bd886eb70d7dde7992ce
Alapakam Dorababu <dorababua67@gmail.com>
2020-02-16 13:05:28 +0530

    All class based views converted into Generic class based views

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,9 @@ from blog import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name="blog-home"),
     path('post/create/', views.CreatePostView.as_view(), name="create-post"),
-    path('post/delete/<int:id>/', views.PostDeleteView.as_view(), name="delete-post"),
-    path('post/<int:id>/', views.PostDetailView.as_view(), name="post-detail"),
+    path('post/detail/<int:pk>/', views.PostDetailView.as_view(), name="post-detail"),
+    path('post/update/<int:pk>/', views.PostUpdateView.as_view(), name="post-update"),
+    path('post/delete/<int:pk>/', views.PostDeleteView.as_view(), name="delete-post"),
     path('author/<str:author>/', views.AuthorPostListView.as_view(), name="author"),
    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from . models import Post
 from . forms import PostForm
 from django.contrib import messages
@@ -6,59 +6,73 @@ from django.core.paginator import Paginator
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.views.generic import View
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View, TemplateView, CreateView, ListView, DetailView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
-
-class HomeView(View):
-    def get(self, request):
-        posts = Post.objects.all().order_by('-date_posted')
-        paginator = Paginator(posts, 3)
-        page_num = request.GET.get('page',1)
-        page = paginator.page(page_num)
-        return render(request, 'blog/home.html', {'page_obj':page})
+class HomeView(ListView):
+    template_name = 'blog/home.html'
+    model = Post        #<queryset = Post.objects.all() or def get_query_set(self)>
+    context_object_name = 'posts'
+    paginate_by = 3
 
 
-class CreatePostView(LoginRequiredMixin ,View):
-    def get(self, request):
-         form = PostForm()
-         return render(request, 'blog/create-post.html', {'form':form})
- 
-    def post(self, request):
-        form = PostForm(request.POST)
-        if form.is_valid():
-            newpost = form.save(commit=False)
-            newpost.author = request.user
-            form.save()
-            messages.success(request, "Your post has been created!")
-            return redirect('post-detail', id=newpost.id)
+class CreatePostView(LoginRequiredMixin, CreateView):
+    template_name = 'blog/create-post.html'
+    model = Post
+    fields = ['title', 'content']
 
-class PostDetailView(View):
-    def get(self, request, id=None):
-        post = Post.objects.get(id=id)
-        return render(request, 'blog/post-details.html', {'post':post})
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return  super().form_valid(form)
 
-class AuthorPostListView(View):
-    def get(self, request, author=None):
+
+class PostDetailView(DetailView):
+    template_name = 'blog/post-details.html'
+    model = Post
+    
+class AuthorPostListView(ListView):
+    template_name = 'blog/author-posts.html'
+    context_object_name = 'posts'
+    paginate_by = 3
+
+    def get_queryset(self):
+        author = get_object_or_404(User, username=self.kwargs.get('author'))
         posts = Post.objects.filter(author__username__startswith = author).order_by('-date_posted')
-        paginator = Paginator(posts, 3)
-        page_num = request.GET.get('page',1)
-        page = paginator.page(page_num)
-        context = {
-            'page_obj':page, 
-            'posts': paginator.count,
-            'username': author,
-        }
-        return render(request, 'blog/author-posts.html', context)
+        return posts
 
-class PostDeleteView(View):
-    def get(self, request, id=None):
-        post = Post.objects.get(id=id)
-        return render(request, 'blog/post-delete.html', {'post':post})
-        
-    def post(self, request, id=None):
-        post = Post.objects.get(id=id)
-        Post.delete(post)
-        return redirect('blog-home')
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        author = get_object_or_404(User, username=self.kwargs.get('author'))
+        posts = Post.objects.filter(author__username__startswith = author).order_by('-date_posted')
+        context['username'] = author
+        context['posts_count'] = posts.count
+        return context
 
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    template_name = 'blog/post-delete.html'
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    template_name = 'blog/create-post.html'
+    model = Post
+    fields = ['title', 'content']
+
+    def test_func(self):
+        post = self.get_object() #getting the current object
+        if self.request.user == post.author:
+            return True
+        return False
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
