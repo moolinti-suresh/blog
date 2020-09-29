796617b8b25dddf6854820ca1d036eb6b6710889
Alapakam Dorababu <dorababua67@gmail.com>
2020-09-29 16:09:05 +0530

    allowed hosts added

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '60%-md(abd4gm%3j*ggf^%_pl!021wb)l3i&=wbkd14kfe_f)e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ad-django-blog.herokuapp.com']
 
 
 # Application definition
